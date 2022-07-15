6840d9fdf16868bd0b6f5e7be553bceb720af498
Dave Cameron <dave@tinyweb.ca>
2022-07-14 21:44:25 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dave Cameron on 2022-07-14.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
